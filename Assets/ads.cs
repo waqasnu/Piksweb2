afdeca1cc8bf3ce240917d2b75ee571ee3a916d2
ImageTech-PC\ImageTech <waqascs@gmail.com>
2017-03-10 22:07:36 +0500

    waqas

---
+++ Assets/ads.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+public class ads : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Advertisement.Show();
+
+		InvokeRepeating("Countdown", 60, 61);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void Countdown () {
+		Advertisement.Show();
+	}
+}
